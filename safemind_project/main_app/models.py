7c3ac7cdcc6c1666eea3deddca658271df46c846
Sarah Elzoubi <sewarrio2000@gmail.com>
2025-10-12 22:09:12 +0300

    Create Model Mood and add it to JournalEntry (many to many)

--- safemind_project/main_app/models.py
+++ safemind_project/main_app/models.py
@@ -6,12 +6,24 @@ from datetime import date
 from django.contrib.auth.models import User
 
 
+
+class Mood(models.Model):
+    name=models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    image = models.ImageField(upload_to='images/', blank=True, null=True)
+
+
+    def __str__(self):
+        return self.name
+    
 class JournalEntry(models.Model):
     user=models.ForeignKey(User,on_delete=models.CASCADE,related_name="Jornal_entries")
     title=models.CharField(max_length=200)
     journal=models.TextField()
     image = models.ImageField(upload_to='images/', blank=True, null=True)  
     created_at=models.DateTimeField(auto_now_add=True)
+    moods = models.ManyToManyField(Mood, blank=True)
 
     def __str__(self):
         return self.title
+
